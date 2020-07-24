ac32aa4efb13a0c239bb50831c6324c07af1f305
lunievas <luisa.nievas94@gmail.com>
2020-07-24 18:55:35 -0300

    criando a função fetchUsers e pegando somente os dados necessários

---
+++ js/script.js
@@ -0,0 +1,28 @@
+const search = document.querySelector('#search');
+const submit = document.querySelector('#submit');
+
+const usersTitle = document.querySelector('#usersTitle');
+const usersFiltred =  document.querySelector('#usersFiltred');
+
+const statisticsTitle =  document.querySelector('#statisticsTitle');
+const statisticsFiltred =  document.querySelector('#statisticsFiltred');
+
+
+
+async function fetchUsers(){
+    const res = await fetch('https://randomuser.me/api/?seed=javascript&results=100&nat=BR&noinfo');
+    const json = await res.json();
+
+    const allUsers = json.results.map((user) =>{
+        const {picture, name, dob, gender} = user;
+
+        return{
+            picture: picture.medium,
+            name: `${name.first} ${name.last}`,
+            age: dob.age,
+            gender,
+        };
+    });
+    console.log(allUsers);
+    return allUsers;
+};
